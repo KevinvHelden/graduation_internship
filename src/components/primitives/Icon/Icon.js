0bcd1426bab8ceece3cbeeb94490c539ce35aa4e
Kevin van Helden <kevin.van.helden@glamorousgoat.nl>
2020-04-28 15:01:27 +0200

    Added all icons used in design

--- src/components/primitives/Icon/Icon.js
+++ src/components/primitives/Icon/Icon.js
@@ -16,7 +16,19 @@ class Icon extends PureComponent {
     /**
      * The image in the icon
      */
-    icon: PropTypes.oneOf(["search", "edit", "delete", "trash"]).isRequired,
+    icon: PropTypes.oneOf([
+      "add",
+      "clear",
+      "delete",
+      "edit",
+      "exit",
+      "filter",
+      "filter_orange",
+      "save",
+      "search",
+      "settings",
+      "view",
+    ]).isRequired,
     /**
      * Wether the icon should be visible or not
      */
@@ -31,11 +43,23 @@ class Icon extends PureComponent {
     const { icon, visible, reference } = this.props;
 
     return (
-      <div ref={reference && reference} className={classnames(styles.root, { [styles.invisible]: !visible })}>
+      <div
+        ref={reference && reference}
+        className={classnames(styles.root, { [styles.invisible]: !visible })}
+      >
         <img
           src={
+            (icon === "add" && Icons.add.src) ||
+            (icon === "clear" && Icons.clear.src) ||
+            (icon === "delete" && Icons.delete.src) ||
+            (icon === "edit" && Icons.edit.src) ||
+            (icon === "exit" && Icons.exit.src) ||
+            (icon === "filter" && Icons.filter.src) ||
+            (icon === "filter_orange" && Icons.filter_orange.src) ||
+            (icon === "save" && Icons.save.src) ||
             (icon === "search" && Icons.search.src) ||
-            (icon === "delete" && Icons.delete.src)
+            (icon === "settings" && Icons.settings.src) ||
+            (icon === "view" && Icons.view.src)
           }
           alt={Icons.search.alt}
         />
--- src/components/primitives/Icon/fixtures/Icons.js
+++ src/components/primitives/Icon/fixtures/Icons.js
@@ -1,9 +1,27 @@
-import searchIcon from "../../../../images/icons/search.svg";
-import deleteIcon from "../../../../images/icons/delete.svg";
+import addIcon from "../../../../images/icons/plus.svg";
+import clearIcon from "../../../../images/icons/cross.svg";
+import deleteIcon from "../../../../images/icons/trashCan.svg";
+import editIcon from "../../../../images/icons/pencil.svg";
+import exitIcon from "../../../../images/icons/exit.svg";
+import filterIcon from "../../../../images/icons/funnel.svg";
+import filterIconOrange from "../../../../images/icons/funnel-orange.svg";
+import saveIcon from "../../../../images/icons/floppyDisk.svg";
+import searchIcon from "../../../../images/icons/magnifyingGlass.svg";
+import settingsIcon from "../../../../images/icons/cog.svg";
+import viewIcon from "../../../../images/icons/eye.svg";
 
 const Icons = { 
-    search: {src: searchIcon, alt: "search"},
+    add: {src: addIcon, alt: "add"},
+    clear: {src: clearIcon, alt: "clear"},
     delete: {src: deleteIcon, alt: "delete"},
+    edit: {src: editIcon, alt: "edit"},
+    exit: {src: exitIcon, alt: "exit"},
+    filter: {src: filterIcon, alt: "filter"},
+    filter_orange: {src: filterIconOrange, alt: "filter"},
+    save: {src: saveIcon, alt: "save"},
+    search: {src: searchIcon, alt: "search"},
+    settings: {src: settingsIcon, alt: "settings"},
+    view: {src: viewIcon, alt: "view"},
 };
 
 export default Icons;
